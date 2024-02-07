e352a2cd283050bc4c88f5aef2e0d19891bfd084
asakura42 <51290160+asakura42@users.noreply.github.com>
2024-02-07 14:52:01 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 URLS=(
-    "https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity.txt"
-    "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/protocols/vmess"
-    "https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/All_Configs_base64_Sub.txt"
-    "https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/v2ray.txt"
-    "https://raw.githubusercontent.com/peasoft/NoMoreWalls/master/list_raw.txt"
+    "plain|https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity.txt"
+    "b64|https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/protocols/vmess"
+    "b64|https://raw.githubusercontent.com/mahdibland/SSAggregator/master/sub/sub_merge_base64.txt"
+    "b64|https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/All_Configs_base64_Sub.txt"
+    "b64|https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/v2ray.txt"
+    "plain|https://raw.githubusercontent.com/peasoft/NoMoreWalls/master/list_raw.txt"
 )
 
 MERGED_FILE="merge.txt"
@@ -14,18 +15,20 @@ FINAL_OUTPUT="output.txt"
 CURL_TIMEOUT=10
 
 fetch_and_decode() {
-    local url="$1"
-    local output_file="$2"
+    local type="$1"
+    local url="$2"
+    local output_file="$3"
 
-    if [[ "$url" =~ base64 ]]; then
+    if [[ "$type" == "b64" ]]; then
         curl -m "$CURL_TIMEOUT" -Ls "$url" | base64 -d >> "$output_file"
     else
         curl -m "$CURL_TIMEOUT" -Ls "$url" >> "$output_file"
     fi
 }
 
-for url in "${URLS[@]}"; do
-    fetch_and_decode "$url" "$MERGED_FILE"
+for item in "${URLS[@]}"; do
+    IFS='|' read -ra ADDR <<< "$item"
+    fetch_and_decode "${ADDR[0]}" "${ADDR[1]}" "$MERGED_FILE"
 done
 
 sort -u "$MERGED_FILE" -o "$MERGED_FILE"
@@ -37,10 +40,9 @@ done | while IFS= read -r line; do
 done >> "$FINAL_OUTPUT"
 
 grep "^ss:" "$MERGED_FILE" | while IFS= read -r line ; do
-    if echo "$line" | grep -oP '(?<=ss:\/\/)[^@]+'  | base64 -d 2>/dev/null | grep -q "2022-blake3\|ietf-poly1305" ; then
+    if echo "$line" | grep -oP '(?<=ss:\/\/)[^@]+' | awk 'length > 80' | base64 -d 2>/dev/null | grep -q "2022-blake3\|ietf-poly1305" ; then
         echo "$line" >> "$FINAL_OUTPUT"
     fi
 done
-
 
 sort -u "$FINAL_OUTPUT" -o "$FINAL_OUTPUT"
