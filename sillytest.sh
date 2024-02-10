b3b37598f88106ce2f36ddc119567f03d8bbc369
asakura42 <51290160+asakura42@users.noreply.github.com>
2024-02-10 18:41:22 +0000

    Update sillytest.sh

--- sillytest.sh
+++ sillytest.sh
@@ -2,8 +2,9 @@
 curl -LO https://github.com/xxf098/LiteSpeedTest/releases/download/v0.15.0/lite-linux-amd64-v0.15.0.gz
 gzip -dk lite-linux-amd64-v0.15.0.gz
 chmod +x ./lite-linux-amd64-v0.15.0
-echo '{ "concurrency":4, "outputMode": 3 }' > config.json
+echo '{ "concurrency":2, "outputMode": 3 }' > config.json
 ./lite-linux-amd64-v0.15.0 -config config.json -test ./output.txt
 cat output.json | jq -r '.nodes[]|select(.max_speed > 0) | .link' > output.txt
+
 ./lite-linux-amd64-v0.15.0 -config config.json -test ./output_443.txt
 cat output.json | jq -r '.nodes[]|select(.max_speed > 0) | .link' > output_443.txt
