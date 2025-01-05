a1457cf9e99b1b7a3e9ce50733b25758167773fa
asakura42 <51290160+asakura42@users.noreply.github.com>
2025-01-05 11:46:49 +0000

    Update sillytest.sh

--- sillytest.sh
+++ sillytest.sh
@@ -3,7 +3,6 @@
 # Download and prepare the LiteSpeedTest tool
 curl -LO https://github.com/lilendian0x00/xray-knife/releases/download/v2.14.21/Xray-knife-linux-64.zip
 unzip Xray-knife-linux-64.zip
-./xray-knife net http -f output.txt -v -x csv -o valid.csv -t 3
-# Process both output files
-cat valid.csv | awk -F',' '$2 == "passed"' | awk -F',' '{print $1}' > output.txt
-cat valid.csv | awk -F',' '$2 == "passed" && $3 == "tls"' | awk -F',' '{print $1}' > output_443.txt
+./xray-knife net http -f output.txt -x csv -o valid.csv --rip --speedtest --sort false > debug.txt
+sed -i '1d' valid.csv
+python best.py > output.txt
