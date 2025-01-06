9e96334de99a60bfbb1f8a4040db2d9a7079625b
asakura42 <51290160+asakura42@users.noreply.github.com>
2025-01-06 10:56:02 +0000

    Update sillytest.sh

--- sillytest.sh
+++ sillytest.sh
@@ -5,6 +5,6 @@ curl -LO https://github.com/lilendian0x00/xray-knife/releases/download/v2.14.21/
 unzip Xray-knife-linux-64.zip -x README.md
 ./xray-knife net http -f output.txt -x csv -o valid.csv -t 16 --amount 10 --mdelay 5000
 cat valid.csv | awk -F',' '$2 == "passed" {print $1}'  > output.txt
-./xray-knife net http -f output.txt -x csv -o valid.csv --rip --sort false --amount 10 --mdelay 5000 --thread 16 | tee debug.txt
+./xray-knife net http -f output.txt -x csv -o valid.csv --rip --sort false --amount 10 --mdelay 5000 --thread 16 > tee debug.txt
 sed -i '1d' valid.csv
 python best.py > output.txt
